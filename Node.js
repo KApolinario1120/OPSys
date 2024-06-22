4b2a85d8a7e64efec3e599b66c4d455f5d3b708a
KApolinario1120 <kevinapolinario.dev@gmail.com>
2024-06-22 06:16:05 -0500

    connection testing Node.js

--- Node.js
+++ Node.js
@@ -3,5 +3,5 @@ var http = require('http');
 
 http.createServer(function(req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('test');
+    res.end('Connection Successful');
 }).listen(8080);
