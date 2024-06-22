f7e330201925b6ee9126b11db830235d09ff2e6d
KApolinario1120 <kevinapolinario.dev@gmail.com>
2024-06-22 09:07:16 -0500

    Add express

--- Node.js
+++ Node.js
@@ -5,3 +5,6 @@ http.createServer(function(req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.end('Connection Successful');
 }).listen(8080);
+
+const x = fetch(http.STATUS_CODES);
+console.log(x);
