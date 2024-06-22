282afc4b9169794f4d1cda39fe33a6f2e8ee8bba
KApolinario1120 <kevinapolinario.dev@gmail.com>
2024-06-22 09:29:32 -0500

    Add TODO and testing endpoint

--- server.js
+++ server.js
@@ -2,6 +2,22 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+app.get('/', (req, res) =>{
+    res.status(200);
+    res.send("Connected to root URL of server");
+});
+
+app.get('/hello', (req, res) =>{
+    res.set('Content-Type', 'text/html');
+    res.status(200).send("<h1>Hello! This is a formatted response!</h1>");
+});
+
+app.post('/submit-order', (req, res) =>{
+    const order = req.body;
+    // TODO - setup API Gateway and connect endpoint here
+    res.status(200).send({ message: 'Order Recieved', order });
+});
+
 app.listen(PORT, (error) =>{
     if(!error)
         console.log("Server is successfully running, and app is listening on port " + PORT)
